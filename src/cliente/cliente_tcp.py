d7a44d5f53f22c498063e7617ed6c4e3ae1485f1
Liedson <83044158+Liedson1@users.noreply.github.com>
2025-03-08 11:29:38 -0300

    Update cliente_tcp.py

--- src/cliente/cliente_tcp.py
+++ src/cliente/cliente_tcp.py
@@ -4,22 +4,22 @@ def cliente_tcp():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("localhost", 65432))
 
-    # Solicita o CPF do usuário
+    
     cpf = input("\n=== Cliente TCP ===\nDigite o CPF ou 'STOP' para interromper o servidor: ")
     
-    # Envia o CPF para o servidor
+    
     client.sendall(cpf.encode())
 
     if cpf == "STOP":
-        # Se for 'STOP', espera a confirmação de que o servidor foi fechado
+        
         resposta = client.recv(1024).decode()
         print("Resposta do servidor:", resposta)
     else:
-        # Recebe a resposta do servidor
+        
         resposta = client.recv(1024).decode()
         print("Resposta do servidor:", resposta)
     
-    # Fecha a conexão com o servidor
+    
     client.close()
 
 if __name__ == "__main__":
