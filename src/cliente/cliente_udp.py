07a8f9a3e9e7054e6a8f07b2e79463cc5503ee6a
Liedson <83044158+Liedson1@users.noreply.github.com>
2025-03-08 11:29:17 -0300

    Update cliente_udp.py

--- src/cliente/cliente_udp.py
+++ src/cliente/cliente_udp.py
@@ -3,22 +3,22 @@ import socket
 def cliente_udp():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     
-    # Solicita o CPF do usuário
+    
     cpf = input("\n=== Cliente UDP ===\nDigite o CPF ou 'STOP' para interromper o servidor: ")
     
-    # Envia o CPF para o servidor
-    client.sendto(cpf.encode(), ("localhost", 65435))  # Envia para o servidor na porta 65433
+    
+    client.sendto(cpf.encode(), ("localhost", 65435))  
     
     if cpf == "STOP":
-        # Se for 'STOP', espera a confirmação de que o servidor foi fechado
+        
         resposta, addr = client.recvfrom(1024)
         print("Resposta do servidor:", resposta.decode())
     else:
-        # Recebe a resposta do servidor
-        resposta, addr = client.recvfrom(1024)  # Utiliza recvfrom em vez de recv, já que é UDP
+        
+        resposta, addr = client.recvfrom(1024)
         print("Resposta do servidor:", resposta.decode())
     
-    # Fecha o socket do cliente
+   
     client.close()
 
 if __name__ == "__main__":
