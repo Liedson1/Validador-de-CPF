0ac3fd84c28960bc3a13fd38a5e57b59aa37aa4e
Liedson <83044158+Liedson1@users.noreply.github.com>
2025-03-08 11:30:13 -0300

    Update validar_cpf.py

--- src/validar_cpf.py
+++ src/validar_cpf.py
@@ -2,10 +2,10 @@ import re
 
 
 def validar_cpf(cpf):
-    cpf = re.sub(r'\D', '', cpf)  # Remove caracteres não numéricos
+    cpf = re.sub(r'\D', '', cpf)  
     if len(cpf) != 11 or len(set(cpf)) == 1:
         return False
-    # Cálculo dos dígitos verificadores
+    
     def calcular_digito(cpf, peso):
         soma = sum(int(cpf[i]) * (peso - i) for i in range(peso - 1))
         resto = soma % 11
